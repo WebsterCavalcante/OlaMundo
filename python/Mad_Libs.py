8f5d15ce4cf6f62b468f09a8d058ea845924041a
WJosé Webster Lima Cavalcante <jose.webster.cavalcante@gmail.com>
2023-05-02 19:37:46 -0300

    Primeiro Commit!

---
+++ python/Mad_Libs.py
@@ -0,0 +1,12 @@
+print('escolha uma das opções a seguir.')
+print('1.conto de fadas')
+print('2.conto de Sci-fi')
+print('3.conto de epoca')
+escolha = int(input('Digite o número do conto que deseja:'))
+if (escolha == 1):
+    print('Você escolheu o conto de fadas!')
+if (escolha == 2):
+    print('você escolheu o conto de ficção cientifica!')
+if (escolha == 3):
+    print('você escolheu o conto de época!')
+    
